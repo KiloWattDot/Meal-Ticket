dd052346ffddf1aff85b04e09d84dbb7732b32c5
Yasin Munye <cyasinsadiq@gmail.com>
2022-04-11 20:07:47 -0400

    added description for review rating star, num of reviews, need to updaated tags, address and phone number

---
+++ client/src/components/RatingComponent.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+import "../utils/rating.css";
+
+
+export function RatingComponent() {
+  return (
+    <div className="rating-part">
+      
+      {/* <FaStar /> */}
+    </div>
+  );
+}
